27828d97d50ab0903e1508c9062a8d234dbaa311
MarkYom <plkplk161211@gmail.com>
2026-01-22 11:41:56 +0800

    docs：创建规范化管理文档

--- utils/util.js
+++ utils/util.js
@@ -1,3 +1,14 @@
+/**
+ * 工具函数模块 - 通用辅助功能
+ * 
+ * 上游依赖：无
+ * 入口：导出的formatTime函数
+ * 主要功能：时间格式化
+ * 输出：格式化的时间字符串
+ * 
+ * 重要：每当所属的代码发生变化时，必须对相应的文档进行更新操作！
+ */
+
 const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
